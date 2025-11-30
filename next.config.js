c1a182e3a27dae5fb180fe3e705cf963c7619ba8
FaelDev-ux <tiorafa2006@gmail.com>
2025-11-30 17:45:07 -0300

    resoluting

--- next.config.js
+++ next.config.js
@@ -3,10 +3,8 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: [],
     unoptimized: false,
   },
-}
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
